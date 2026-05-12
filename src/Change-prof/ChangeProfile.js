06f6366b7bf06f143795445261c7d374efb0f415
amin <amingholipoor5327@gamil.com>
2026-05-12 11:53:19 +0330

    Edit Change profile page

--- src/Change-prof/ChangeProfile.js
+++ src/Change-prof/ChangeProfile.js
@@ -1,48 +1,36 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import "./change.css";
 
-export const Change = (props)=>{
-    const [changename , setChangename] = useState()
-    const [changeemail , setChangeemail] = useState()
-    const [changeaddres , setChangeaddres] = useState()
-    const [changegit , setChangegit] = useState()
-    
-    return <div>
-        <div>
-        <input type="text" placeholder="name..." onChange={(event)=>{setChangename(event.target.value)}}></input>
-        <button onClick={()=>{props.setName(changename)}}>ویرایش اسم </button>
-        </div>
+export const Change = (props) => {
+  const [changename, setChangename] = useState("");
+  const [changeemail, setChangeemail] = useState("");
+  const [changeaddres, setChangeaddres] = useState("");
+  const [changegit, setChangegit] = useState("");
 
-        <div>
-        <input 
-        type="email"
-         placeholder="email..."
-          onChange={(event)=>{setChangeemail(event.target.value)}}>
-          </input>
-        <button onClick={()=>{props.setEmail(changeemail)}}>ویرایش ایمیل </button>
-        </div>
-        
-        <div>
-        <input 
-        type="text"
-         placeholder="addres..."
-          onChange={(event)=>{setChangeaddres(event.target.value)}}>
-          </input>
-        <button onClick={()=>{props.setAddres(changeaddres)}}>ویرایش ادرس </button>
-        </div>
+  return (
+    <div className="change-container">
+      <div className="form-group">
+        <input type="text" placeholder="name..." onChange={(event) => setChangename(event.target.value)} />
+        <button onClick={() => props.setName(changename)}>ویرایش اسم</button>
+      </div>
 
-        <div>
-        <input 
-        type="text"
-         placeholder="github..."
-          onChange={(event)=>{setChangegit(event.target.value)}}>
-          </input>
-        <button onClick={()=>{props.setGithub(changegit)}}>ویرایش گیت   </button>
-        </div>
-        <div>
-            <Link to={"/"}>close</Link>
-        </div>
-        </div>
+      <div className="form-group">
+        <input type="email" placeholder="email..." onChange={(event) => setChangeemail(event.target.value)} />
+        <button onClick={() => props.setEmail(changeemail)}>ویرایش ایمیل</button>
+      </div>
 
-     
-}
+      <div className="form-group">
+        <input type="text" placeholder="address..." onChange={(event) => setChangeaddres(event.target.value)} />
+        <button onClick={() => props.setAddres(changeaddres)}>ویرایش آدرس</button>
+      </div>
+
+      <div className="form-group">
+        <input type="text" placeholder="github..." onChange={(event) => setChangegit(event.target.value)} />
+        <button onClick={() => props.setGithub(changegit)}>ویرایش گیت</button>
+      </div>
+
+      <Link to={"/"} className="close-link">close</Link>
+    </div>
+  );
+};
